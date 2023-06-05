78c415c4438ea3a76ee64aa4b18a4445033b53ac
tabares9002 <tabares9002@gmail.com>
2023-06-05 07:19:45 -0300

    Correcciones de método find

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-var productos = [
-    {nombre: "Teclado", precio: 10},
-    {nombre: "Mouse", precio: 8},
-    {nombre: "Parlantes", precio: 5},
-    {nombre: "Monitor", precio: 150},
-    {nombre: "Torre", precio: 80}
+let productos = [
+    { nombre: "Teclado", precio: 10 },
+    { nombre: "Mouse", precio: 8 },
+    { nombre: "Parlantes", precio: 5 },
+    { nombre: "Monitor", precio: 150 },
+    { nombre: "Torre", precio: 80 }
 ];
 
-var nombreProducto = prompt("Ingrese el nombre del producto:");
+let nombreProducto = prompt("Ingrese el nombre del producto:");
 nombreProducto = nombreProducto.toLowerCase();
 
-var productoEncontrado = productos.find
-
-(function (producto) {
-    return producto.nombre.toLowerCase() === nombreProducto;
-});
+let productoEncontrado = productos.find(producto => producto.nombre.toLowerCase() === nombreProducto
+);
 
 if (productoEncontrado) {
     alert("El precio del producto" + " " + productoEncontrado.nombre + " es de: " + "U$S" + " " + productoEncontrado.precio);
